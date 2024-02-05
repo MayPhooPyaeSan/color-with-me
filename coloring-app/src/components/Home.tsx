f641f401bee5acac5611ee2d6db09a91ed97074f
May Phoo Pyae San <sakuramay1210@gmail.com>
2024-02-06 01:35:14 +0630

    added 6 colors

--- coloring-app/src/components/Home.tsx
+++ coloring-app/src/components/Home.tsx
@@ -7,6 +7,12 @@ const Home: React.FC = () => {
   const colors4 = ["344e41", "3a5a40", "588157", "a3b18a", "84a98c"];
   const colors5 = ["590d22", "800f2f", "a4133c", "ff4d6d", "ff758f"];
   const colors6 = ["ff6d00", "ff7900", "ff8500", "ff9100", "ff9e00"];
+  const colors7 = ["231942", "5e548e", "9f86c0", "be95c4", "e0b1cb"];
+  const colors8 = ["461220", "8c2f39", "b23a48", "fcb9b2", "fed0bb"];
+  const colors9 = ["ef6351", "f38375", "f7a399", "fbc3bc", "ffe3e0"];
+  const colors10 = ["ff595e", "ffca3a", "8ac926", "1982c4", "6a4c93"];
+  const colors11 = ["eb5e28", "252422", "403d39", "ccc5b9", "fffcf2"];
+  const colors12 = ["0d3b66", "faf0ca", "f4d35e", "ee964b", "f95738"];
 
   return (
     <div className="ColorPalette" style={{ padding: "20px" }}>
@@ -16,6 +22,12 @@ const Home: React.FC = () => {
       <ColorPalette colors={colors4} />
       <ColorPalette colors={colors5} />
       <ColorPalette colors={colors6} />
+      <ColorPalette colors={colors7} />
+      <ColorPalette colors={colors8} />
+      <ColorPalette colors={colors9} />
+      <ColorPalette colors={colors10} />
+      <ColorPalette colors={colors11} />
+      <ColorPalette colors={colors12} />
     </div>
   );
 };
