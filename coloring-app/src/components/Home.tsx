d54dd4c6a13ddd4438c62d8c42f738f4005c97df
May Phoo Pyae San <sakuramay1210@gmail.com>
2024-02-06 02:36:40 +0630

    added total 30 colors

--- coloring-app/src/components/Home.tsx
+++ coloring-app/src/components/Home.tsx
@@ -13,6 +13,24 @@ const Home: React.FC = () => {
   const colors10 = ["ff595e", "ffca3a", "8ac926", "1982c4", "6a4c93"];
   const colors11 = ["eb5e28", "252422", "403d39", "ccc5b9", "fffcf2"];
   const colors12 = ["0d3b66", "faf0ca", "f4d35e", "ee964b", "f95738"];
+  const colors13 = ["114b5f", "1a936f", "88d498", "c6dabf", "f3e9d2"];
+  const colors14 = ["006d77", "83c5be", "edf6f9", "ffddd2", "e29578"];
+  const colors15 = ["ffba08", "faa307", "f48c06", "e85d04", "dc2f02"];
+  const colors16 = ["f4f1de", "e07a5f", "3d405b", "81b29a", "f2cc8f"];
+  const colors17 = ["f28482", "84a59d", "f5cac3", "f7ede2", "f6bd60"];
+  const colors18 = ["fec89a", "ffd7ba", "ffe5d9", "ece4db", "d8e2dc"];
+  const colors19 = ["606c38", "283618", "fefae0", "dda15e", "bc6c25"];
+  const colors20 = ["1d3557", "f1faee", "a8dadc", "457b9d", "e63946"];
+  const colors21 = ["f72585", "b5179e", "7209b7", "480ca8", "3a0ca3"];
+  const colors22 = ["adb5bd", "6c757d", "495057", "343a40", "212529"];
+  const colors23 = ["f8f9fa", "e9ecef", "dee2e6", "ced4da", "dad7cd"];
+  const colors24 = ["FDF0D1", "AC7D88", "85586F", "643843", "561C24"];
+  const colors25 = ["FE7A36", "3652AD", "280274", "E9F6FF", "F5EEE6"];
+  const colors26 = ["030637", "3C0753", "720455", "910A67", "492E87"];
+  const colors27 = ["DCFFB7", "FF6868", "720455", "FFBB64", "FFEAA7"];
+  const colors28 = ["aa998f", "d1be9c", "7d4f50", "f9eae1", "cc8b86"];
+  const colors29 = ["253237", "5c6b73", "9db4c0", "c2dfe3", "e0fbfc"];
+  const colors30 = ["DC84F3", "E9A8F2", "F3CCF3", "F3CCF3", "FEFAE0"];
 
   return (
     <div className="ColorPalette" style={{ padding: "20px" }}>
@@ -28,6 +46,24 @@ const Home: React.FC = () => {
       <ColorPalette colors={colors10} />
       <ColorPalette colors={colors11} />
       <ColorPalette colors={colors12} />
+      <ColorPalette colors={colors13} />
+      <ColorPalette colors={colors14} />
+      <ColorPalette colors={colors15} />
+      <ColorPalette colors={colors16} />
+      <ColorPalette colors={colors17} />
+      <ColorPalette colors={colors18} />
+      <ColorPalette colors={colors19} />
+      <ColorPalette colors={colors20} />
+      <ColorPalette colors={colors21} />
+      <ColorPalette colors={colors22} />
+      <ColorPalette colors={colors23} />
+      <ColorPalette colors={colors24} />
+      <ColorPalette colors={colors25} />
+      <ColorPalette colors={colors26} />
+      <ColorPalette colors={colors27} />
+      <ColorPalette colors={colors28} />
+      <ColorPalette colors={colors29} />
+      <ColorPalette colors={colors30} />
     </div>
   );
 };
